4a16476f5a4a84888b7c81f9302f8c6814951517
Normando Ramírez <nramirezd@cbtis236.edu.mx>
2025-02-18 09:02:12 -0600

    .

--- 14_conjuntos_set.dart
+++ 14_conjuntos_set.dart
@@ -1,9 +1,10 @@
 void main() {
   var numeros = {1, 2, 3, 4, 5}; // Set de enteros
-  //print(numeros); // {1, 2, 3, 4, 5}
+  print(numeros); // {1, 2, 3, 4, 5}
 
 
-  Set<String> vacio = Set(); // Crea un
+  Set<String> vacio = Set(); // Crea un Conjunto Vacio
+  print(vacio);
 
   Set<int> enteros = {1, 2, 3, 4, 5, 5}; // Se eliminan los duplicados automáticamente
   print(enteros); // {1, 2, 3, 4, 5}
